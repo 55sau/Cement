48d482fa49c9abc49ff421246e019f524606cc98
55sau <124778076+55sau@users.noreply.github.com>
2023-03-07 11:13:49 +0530

    Add files via upload

--- untitled.py
+++ untitled.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
 
-st.title('CEMENT SALES AND DEMAND FORECASTING')
+st.title('CEMENT DEMAND FORECASTING')
 uploaded_file = st.file_uploader(" ", type=['xlsx'])
 
 if uploaded_file is not None:     
@@ -14,13 +14,9 @@ if uploaded_file is not None:
     
     st.subheader("EXPONENTIAL MODEL")
    
-    st.write("DEMAND FORECAST:", newdata_pred)
+    st.write("DEMAND FORECASTING:", newdata_pred)
     
-   # hwe_model_mul_add = ExponentialSmoothing(data["Cement_Demand"][:1329], seasonal = "mul", trend = "add", seasonal_periods = 12).fit()
-    
-    #newdata_pred = hwe_model_mul_add.predict(start = data.index[0], end = data.index[-1])
    
-    #st.write("DEMAND FORECAST:",newdata_pred)
     
     
     
